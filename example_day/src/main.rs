908fe24c58136a9103e0a4604d852c2192029602
Xavier Bick <fxb9500@gmail.com>
2019-12-02 20:43:49 -0600

    Again make the linter happy

--- example_day/src/main.rs
+++ example_day/src/main.rs
@@ -3,6 +3,6 @@ use puzzlehandler;
 fn main() {
     let input = puzzlehandler::ints_from_csv("input.csv".to_string());
     let input2 = puzzlehandler::ints_from_csv("input2.csv".to_string());
-    println!("{:?}", input.unwrap());
-    println!("{:?}", input2.unwrap());
+    puzzlehandler::resolve(Box::new(input.unwrap()));
+    puzzlehandler::resolve(Box::new(input2.unwrap()));
 }
