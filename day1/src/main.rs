daecf11132dd6721dda01cfd9e46be8686563a49
Xavier Bick <fxb9500@gmail.com>
2019-12-02 19:52:35 -0600

    Finish Day 1 and Day 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,6 +1,52 @@
 use puzzlehandler;
 
 fn main() {
-    let input = puzzlehandler::ints_from_csv("input.csv".to_string());
-    println!("{:?}", input.unwrap());
+    let input = puzzlehandler::ints_from_csv("input.csv".to_string()).unwrap();
+    let result = compute_answer(input.clone());
+    println!("Part 1: {:?}", result);
+    let result2 = compute_answer_part2(input);
+    println!("Part 2: {:?}", result2);
+}
+
+fn compute_answer(modules: Vec<i32>) -> i32 {
+    let mut result: i32 = 0;
+    for module in modules {
+        result += (module / 3) - 2;
+    }
+    return result;
+}
+
+fn compute_answer_part2(modules: Vec<i32>) -> i32 {
+    let mut result: i32 = 0;
+    for module in modules {
+        let mut fuel_for_module = (module / 3) - 2;
+        let mut remaining_fuel_to_account_for = fuel_for_module;
+
+        while remaining_fuel_to_account_for >= 0 {
+            remaining_fuel_to_account_for = (remaining_fuel_to_account_for / 3) - 2;
+            if remaining_fuel_to_account_for >= 0 {
+                fuel_for_module += remaining_fuel_to_account_for;
+            }
+        }
+
+        result += fuel_for_module;
+    }
+    return result;
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn compute_answer_works() {
+        let modules = vec![12, 14, 1969, 100756];
+        let result = super::compute_answer(modules);
+        assert_eq!(result, 34241);
+    }
+
+    #[test]
+    fn compute_answer_part2_works() {
+        let modules = vec![12, 14, 1969, 100756];
+        let result = super::compute_answer_part2(modules);
+        assert_eq!(result, 51316);
+    }
 }
--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,60 @@
 fn main() {
-    println!("Hello, world!");
+    let input = puzzlehandler::ints_from_csv("input.csv".to_string()).unwrap();
+    let mut intcode = input.clone();
+    intcode[1] = 12;
+    intcode[2] = 2;
+    let result = compute_answer(intcode);
+    println!("Part 1: {:?}", result);
+
+    let mut noun = 0;
+    while noun < 100 {
+        let mut verb = 0;
+        while verb < 100 {
+            let mut intcode = input.clone();
+            intcode[1] = noun;
+            intcode[2] = verb;
+            let result = compute_answer(intcode);
+            if result == 19690720 {
+                println!("Part 2: {:?}{:?}", noun, verb);
+                return;
+            }
+            verb += 1;
+        }
+        noun += 1;
+    }
+    println!("No solution found.");
+    panic!();
+}
+
+fn compute_answer(input: Vec<i32>) -> i32 {
+    let mut intcode = input.clone();
+    let mut i = 0;
+    while i < input.len() {
+        match intcode[i] {
+            1 => {
+                let change_index = intcode[i+3] as usize;
+                intcode[change_index] = intcode[intcode[i+1] as usize] + intcode[intcode[i+2] as usize];
+            },
+            2 => {
+                let change_index = intcode[i+3] as usize;
+                intcode[change_index] = intcode[intcode[i+1] as usize] * intcode[intcode[i+2] as usize];
+            },
+            99 => {
+                break;
+            },
+            _ => {}
+        }
+        i += 4;
+    }
+    return intcode[0];
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn compute_answer_works() {
+        let input = vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50];
+        let result = super::compute_answer(input);
+        assert_eq!(result, 3500);
+    }
 }
